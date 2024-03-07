d512755424b841535c1b279e9e64214ae018bcbe
Lucas Máximo <lucasmaximo@outlook.com>
2024-03-07 11:06:56 -0300

    Tratando erros com excecao personalizada

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -7,16 +7,23 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            //instanciando um tabuleiro de tamanho 8x8
-            Tabuleiro tabuleiro = new Tabuleiro(8, 8);
+            try
+            {
+                //instanciando um tabuleiro de tamanho 8x8
+                Tabuleiro tabuleiro = new Tabuleiro(8, 8);
 
-            //coloca as peças no tabuleiro, apontando sua cor e sua posição
-            tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(0, 0));
-            tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(0, 1));
-            tabuleiro.ColocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(2, 4));
+                //coloca as peças no tabuleiro, apontando sua cor e sua posição
+                tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(0, 0));
+                tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(0, 9));
+                tabuleiro.ColocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(0, 2));
 
-            //imprimindo o tabuleiro na tela
-            Tela.ImprimirTabuleiro(tabuleiro);
+                //imprimindo o tabuleiro na tela
+                Tela.ImprimirTabuleiro(tabuleiro);
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
 
             Console.ReadLine();
         }
--- xadrez-console/tabuleiro/Tabuleiro.cs
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -12,23 +12,61 @@ namespace tabuleiro
         public int Colunas { get; set; }
         private Peca[,] Pecas { get; set; }
 
-        public Tabuleiro(int linhas, int colunas) { 
+        public Tabuleiro(int linhas, int colunas)
+        {
             Linhas = linhas;
             Colunas = colunas;
-            Pecas = new Peca[linhas,colunas];
+            Pecas = new Peca[linhas, colunas];
         }
 
         //retorna a peça ou o espaço vazio dentro da matriz de peças quando imprimimos o tabuleiro na tela
         public Peca Peca(int linha, int coluna)
         {
-            return Pecas[linha,coluna];
+            return Pecas[linha, coluna];
         }
+
+        //sobrecarga do método Peca, mas recetornando uma posição dentro da matriz de Pecas
+        public Peca Peca(Posicao posicao)
+        {
+            return Pecas[posicao.Linha, posicao.Coluna];
+        }
+
+        //verifica se existe uma peça em uma determinada posição
+        public bool ExistePeca(Posicao posicao)
+        {
+            ValidarPosicao(posicao);
+            return Peca(posicao) != null;
+        }
+
 
         //coloca uma peça dentro da matriz de peças do tabuleiro e atribui a posição àquela peça
         public void ColocarPeca(Peca peca, Posicao posicao)
         {
+            if (ExistePeca(posicao))
+            {
+                throw new TabuleiroException("Já existe uma peça nessa posição!");
+            }
             Pecas[posicao.Linha, posicao.Coluna] = peca;
             peca.Posicao = posicao;
+        }
+
+        //testa se uma posição é válida
+        public bool PosicaoValida(Posicao posicao)
+        {
+            if (posicao.Linha < 0 || posicao.Linha > Linhas || posicao.Coluna < 0 || posicao.Coluna > Colunas)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        //trata erros de posições das peças de maneira personalizada
+        public void ValidarPosicao(Posicao posicao)
+        {
+            if (!PosicaoValida(posicao))
+            {
+                throw new TabuleiroException("Posição inválida!");
+            }
         }
     }
 }
